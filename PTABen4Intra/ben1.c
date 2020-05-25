d63bdd95b140d2a71530e9c2236b2802ebae5180
shrep16 <prerona.chaudhuri16@vit.edu>
2020-05-25 23:08:13 +0530

    Adding PTABen benchmarks

---
+++ PTABen4Intra/ben1.c
@@ -0,0 +1,37 @@
+#include "myHeader.h"
+int *a, *b, *w;
+int **c, **s;
+int x, y;
+int u;
+int main(){
+
+    
+    a=&x;
+    b=&y;
+    c=&a;
+    s = c;
+    mustPointsTo(s,a);
+    //s=&w;
+    w=&u;
+    mustPointsTo(w,u);
+    u = 9;
+     
+    if(x==5){
+    	mustPointsTo(c,a);
+	mustPointsTo(a,x);
+    	**c = 10;
+    }
+    else{
+	*b = 100;
+    }
+
+    mustPointsTo(c,a);
+    while(u>10){
+	mustPointsTo(s,a);
+	mayPointsTo(s,a);
+	**s = 100;
+    }
+
+    return 0;
+
+}
